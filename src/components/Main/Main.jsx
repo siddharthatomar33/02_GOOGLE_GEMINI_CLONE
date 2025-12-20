ff8c5aa5228a5bbd0f845c86556a5256ffbd3562
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-12-20 20:35:26 +0530

    gemini-cloneday2

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,7 +9,18 @@ const Main = () => {
             <p>Gemini</p>
             <img src={assets.user_icon} alt="" />
         </div>
-        
+      <div className="main-container">
+        <div className="greet">
+            <p><span>Hello, Dev.</span></p>
+            <p>How can Ihelp you today?</p>
+        </div>
+        <div className="cards">
+            <div className="card">
+                <p>Suggest beautiful palce to see on an upcoming travel trip</p>
+                <img src={assets.compass_icon} alt="" />
+            </div>
+        </div>
+        </div>  
     </div>
   )
 }
