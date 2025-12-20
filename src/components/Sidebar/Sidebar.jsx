5c72b8eaae54e3d77f0a0720ee9697cb760a524e
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-12-20 19:43:48 +0530

    gemini-cloneday2

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Sidebar.css'
 import {assets} from '../../assets/assets'
 
 const Sidebar = () => {
+
+  const[extended,setExtended] = useState(false)
+
   return (
     <div className='sidebar'>
        <div className="top">
           <img className='menu' src={assets.menu_icon} alt="" />
           <div className="new-chat">
             <img src={assets.plus_icon} alt="" />
-            <p>New Chat</p>
+            {extended?<p>New Chat</p>:"null"}
           </div>
-          <div className="recent">
+          {extended? <div className="recent">
             <p className="recent-title">Recent</p>
             <div className="recent-entry">
               <img src={assets.message_icon} alt="" />
               <p>what is react....</p>
             </div>
-          </div>
+          </div> : "null"}
+          
        </div>
        <div className="bottom">
          <div className="bottom-item recent-entry" >
@@ -39,4 +43,4 @@ const Sidebar = () => {
   )
 }
 
-export default Sidebbar
+export default Sidebar
